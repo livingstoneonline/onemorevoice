1f4e3626beb658d576c45a98e58773b49cfc22f9
Adrian Wisnicki <awisnicki@yahoo.com>
2020-05-26 16:26:45 -0500

    create overlay js

---
+++ testing-site/0_dropdown-test-5/overlay.js
@@ -0,0 +1,9 @@
+/* Taken from https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp */
+
+        function openNav() {
+          document.getElementById("myNav").style.display = "block";
+        }
+        
+        function closeNav() {
+          document.getElementById("myNav").style.display = "none";
+        }

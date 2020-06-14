3937b661a7c089f9fdfd943b6f39374962311896
Adrian Wisnicki <awisnicki@yahoo.com>
2020-06-14 12:07:14 -0500

    add dev site

---
+++ dev-site/overlay.js
@@ -0,0 +1,25 @@
+/* Taken from https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp */
+
+        function openNav() {
+          document.getElementById("nav3").style.display = "block";
+          document.getElementById("title-div").style.display = "none";
+          document.getElementById("main-div").style.display = "none";
+          document.getElementById("manuscript-div").style.display = "none";
+          document.getElementById("journal-div").style.display = "none";
+          document.getElementById("credits-div").style.display = "none";
+          document.getElementById("credits1-div").style.display = "none";
+          document.getElementById("credits2-div").style.display = "none";
+          document.getElementById("footer-div").style.display = "none";
+        }
+        
+        function closeNav() {
+          document.getElementById("nav3").style.display = "none";
+          document.getElementById("title-div").style.display = "block";
+          document.getElementById("main-div").style.display = "block";
+          document.getElementById("manuscript-div").style.display = "block";
+          document.getElementById("journal-div").style.display = "block";
+          document.getElementById("credits-div").style.display = "block";
+          document.getElementById("credits1-div").style.display = "block";
+          document.getElementById("credits2-div").style.display = "block";
+          document.getElementById("footer-div").style.display = "block";
+        }

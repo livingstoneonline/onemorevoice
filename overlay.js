68659e700a84d7506760cd3fcbf6cf7517d17509
Adrian Wisnicki <awisnicki@yahoo.com>
2020-06-15 14:16:56 -0500

    update overlay to hide all background elements

--- overlay.js
+++ overlay.js
@@ -1,25 +1,24 @@
-/* Taken from https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp */
+/* Adapted from https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp */
+/* This works, but there is probably a much more concise way to write this. */
 
-        function openNav() {
-          document.getElementById("nav3").style.display = "block";
-          document.getElementById("title-div").style.display = "none";
-          document.getElementById("main-div").style.display = "none";
-          document.getElementById("manuscript-div").style.display = "none";
-          document.getElementById("journal-div").style.display = "none";
-          document.getElementById("credits-div").style.display = "none";
-          document.getElementById("credits1-div").style.display = "none";
-          document.getElementById("credits2-div").style.display = "none";
-          document.getElementById("footer-div").style.display = "none";
-        }
-        
-        function closeNav() {
-          document.getElementById("nav3").style.display = "none";
-          document.getElementById("title-div").style.display = "block";
-          document.getElementById("main-div").style.display = "block";
-          document.getElementById("manuscript-div").style.display = "block";
-          document.getElementById("journal-div").style.display = "block";
-          document.getElementById("credits-div").style.display = "block";
-          document.getElementById("credits1-div").style.display = "block";
-          document.getElementById("credits2-div").style.display = "block";
-          document.getElementById("footer-div").style.display = "block";
-        }
+function openNav() {
+  document.getElementById("nav3").style.display = "block";
+  if (document.getElementById("title-div") !== null) {document.getElementById("title-div").style.display = "none"};
+  if (document.getElementById("main-div") !== null) {document.getElementById("main-div").style.display = "none"};
+  if (document.getElementById("manuscript-div") !== null) {document.getElementById("manuscript-div").style.display = "none"};
+  if (document.getElementById("journal-div") !== null) {document.getElementById("journal-div").style.display = "none"};
+  if (document.getElementById("credits-div") !== null) {document.getElementById("credits-div").style.display = "none"};
+  if (document.getElementById("credits1-div") !== null) {document.getElementById("credits1-div").style.display = "none"};
+  if (document.getElementById("credits2-div") !== null) {document.getElementById("credits2-div").style.display = "none"};
+}
+
+function closeNav() {
+  document.getElementById("nav3").style.display = "none";
+  if (document.getElementById("title-div") !== null) {document.getElementById("title-div").style.display = "block"};
+  if (document.getElementById("main-div") !== null) {document.getElementById("main-div").style.display = "block"};
+  if (document.getElementById("manuscript-div") !== null) {document.getElementById("manuscript-div").style.display = "block"};
+  if (document.getElementById("journal-div") !== null) {document.getElementById("journal-div").style.display = "block"};
+  if (document.getElementById("credits-div") !== null) {document.getElementById("credits-div").style.display = "block"};
+  if (document.getElementById("credits1-div") !== null) {document.getElementById("credits1-div").style.display = "block"};
+  if (document.getElementById("credits2-div") !== null) {document.getElementById("credits2-div").style.display = "block"};
+}

a613893cd9dc318c360cce2f1628a16cd1a8b8e1
Adrian Wisnicki <awisnicki@yahoo.com>
2020-06-30 11:43:52 -0500

    testing rotate js

--- rotate.js
+++ rotate.js
@@ -1,6 +1,6 @@
 function myFunction() {
   var element1 = document.getElementById("rotate-button");
-  var element2 = document.getElementById("image-to-rotate");
+  var element2 = document.getElementById("img0016-0017");
   if (element1.getAttribute("aria-checked") == "true") {
       element1.setAttribute("aria-checked", "false");
   } else {

e26da716760f200be0bd0fe133be7a47d9edfb2d
Adrian Wisnicki <awisnicki@yahoo.com>
2020-06-30 11:14:46 -0500

    add rotate js

---
+++ rotate.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  var element1 = document.getElementById("rotate-button");
+  var element2 = document.getElementById("image-to-rotate");
+  if (element1.getAttribute("aria-checked") == "true") {
+      element1.setAttribute("aria-checked", "false");
+  } else {
+      element1.setAttribute("aria-checked", "true");
+  }
+  element1.classList.toggle("click-color");
+  element2.classList.toggle("rotate-180");
+}

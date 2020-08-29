5075ed2465ace4d8bce22a738677c463f2746e96
Adrian Wisnicki <awisnicki@yahoo.com>
2020-08-29 14:57:13 -0500

    update scripts for multiple switches

--- scripts.js
+++ scripts.js
@@ -239,20 +239,48 @@ function getPreviousElement(items, currentItem){
 }
 
 
-/* Toggle Color-B&W TEI Rendering */
+/* Color, Inversion, Rotation Switches */
 /* Created for One More Voice by way of Livingstone Online by Nigel Banks */
 
 // Wait till document is loaded before executing.
 document.addEventListener("DOMContentLoaded", function(event) {
   // Select the button element using it's ID.
-  document.querySelector('input#on-off-formatting')
+  document.querySelector('input#remove-color')
     // Call this function when the button is clicked.
     .onclick = function () {
       // Find the element with class transcription,
       // and toggle the class 'change-display'.
       document.querySelector('body.transcription')
         .classList
-        .toggle('change-display');
+        .toggle('change-color');
+    };
+});
+
+// Wait till document is loaded before executing.
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Select the button element using it's ID.
+  document.querySelector('input#invert')
+    // Call this function when the button is clicked.
+    .onclick = function () {
+      // Find the element with class transcription,
+      // and toggle the class 'change-display'.
+      document.querySelector('body.transcription')
+        .classList
+        .toggle('invert-color');
+    };
+});
+
+// Wait till document is loaded before executing.
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Select the button element using it's ID.
+  document.querySelector('input#remove-rotation')
+    // Call this function when the button is clicked.
+    .onclick = function () {
+      // Find the element with class transcription,
+      // and toggle the class 'change-display'.
+      document.querySelector('body.transcription')
+        .classList
+        .toggle('rotate');
     };
 });
 

036e4c37593816ec32665064b7d79de16538be39
Adrian Wisnicki <awisnicki@yahoo.com>
2020-09-09 14:58:11 -0500

    update scripts to move out switches

--- scripts.js
+++ scripts.js
@@ -239,66 +239,6 @@ function getPreviousElement(items, currentItem){
 }
 
 
-/* Color, Inversion, Rotation Switches */
-/* Created for One More Voice by way of Livingstone Online by Nigel Banks */
-
-// Wait till document is loaded before executing.
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Select the button element using it's ID.
-  document.querySelector('input#remove-color')
-    // Call this function when the button is clicked.
-    .onclick = function () {
-      // Find the element with class transcription,
-      // and toggle the class 'change-display'.
-      document.querySelector('body.transcription')
-        .classList
-        .toggle('change-color');
-    };
-});
-
-// Wait till document is loaded before executing.
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Select the button element using it's ID.
-  document.querySelector('input#invert')
-    // Call this function when the button is clicked.
-    .onclick = function () {
-      // Find the element with class transcription,
-      // and toggle the class 'change-display'.
-      document.querySelector('body.transcription')
-        .classList
-        .toggle('invert-color');
-    };
-});
-
-// Wait till document is loaded before executing.
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Select the button element using it's ID.
-  document.querySelector('input#remove-rotation')
-    // Call this function when the button is clicked.
-    .onclick = function () {
-      // Find the element with class transcription,
-      // and toggle the class 'change-display'.
-      document.querySelector('body.transcription')
-        .classList
-        .toggle('rotate');
-    };
-});
-
-// Wait till document is loaded before executing.
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Select the button element using it's ID.
-  document.querySelector('input#remove-justification')
-    // Call this function when the button is clicked.
-    .onclick = function () {
-      // Find the element with class transcription,
-      // and toggle the class 'change-display'.
-      document.querySelector('body.transcription')
-        .classList
-        .toggle('unjustify');
-    };
-});
-
-
 /* Image Rotation */
 /* Adapted from https://www.w3schools.com/howto/howto_js_toggle_class.asp and https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Switch_role */
 

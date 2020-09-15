43cd1452c62b4a781c6423946efefe2b64c81617
Adrian Wisnicki <awisnicki@yahoo.com>
2020-09-15 10:48:04 -0500

    update scripts to move out image rotation

--- scripts.js
+++ scripts.js
@@ -241,19 +241,3 @@ function getPreviousElement(items, currentItem){
   const currentIndex = items.findIndex(x => x == currentItem);
   return items[Math.max(currentIndex - 1, 0)];
 }
-
-
-/* Image Rotation */
-/* Adapted from https://www.w3schools.com/howto/howto_js_toggle_class.asp and https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Switch_role */
-
-function myFunction() {
-    var element1 = document.getElementById("rotate-button");
-    var element2 = document.getElementById("image-to-rotate");
-    if (element1.getAttribute("aria-checked") == "true") {
-        element1.setAttribute("aria-checked", "false");
-    } else {
-        element1.setAttribute("aria-checked", "true");
-    }
-    element1.classList.toggle("click-color");
-    element2.classList.toggle("rotate-180");
-  }

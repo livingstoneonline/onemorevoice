b6fde4b06b6bcbca8dffa37dfbd019b129dca386
Adrian Wisnicki <awisnicki@yahoo.com>
2020-09-17 08:35:22 -0500

    create ms transform script

---
+++ scripts/mansucript-transform.js
@@ -0,0 +1,44 @@
+/* Color, Inversion, Rotation Switches */
+/* Created for One More Voice by way of Livingstone Online by Nigel Banks */
+
+// Wait till document is loaded before executing.
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Select the button element using it's ID.
+    document.querySelector('input#remove-color')
+    // Call this function when the button is clicked.
+    .onclick = function () {
+        // Find the element with class transcription,
+        // and toggle the class 'change-display'.
+        document.querySelector('body.transcription')
+        .classList
+        .toggle('change-color');
+    };
+});
+
+// Wait till document is loaded before executing.
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Select the button element using it's ID.
+    document.querySelector('input#invert')
+    // Call this function when the button is clicked.
+    .onclick = function () {
+        // Find the element with class transcription,
+        // and toggle the class 'change-display'.
+        document.querySelector('body.transcription')
+        .classList
+        .toggle('invert-color');
+    };
+});
+
+// Wait till document is loaded before executing.
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Select the button element using it's ID.
+    document.querySelector('input#remove-rotation')
+    // Call this function when the button is clicked.
+    .onclick = function () {
+        // Find the element with class transcription,
+        // and toggle the class 'change-display'.
+        document.querySelector('body.transcription')
+        .classList
+        .toggle('rotate');
+    };
+});

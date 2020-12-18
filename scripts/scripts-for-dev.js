07ef98ad8d00df9d5175e575ea709f0d593cfc1a
Adrian Wisnicki <awisnicki@yahoo.com>
2020-12-18 09:19:53 -0600

    update scripts dev

--- scripts/scripts-for-dev.js
+++ scripts/scripts-for-dev.js
@@ -277,19 +277,18 @@ function AddRelNoopener(){
 }
 
 // Adapted from https://stackoverflow.com/a/18704113
-function randomString(length, chars) {
-  var result = '';
-  for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-  return result;
-}
-
 function randomizeHref()
 {
+  function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
+  }
     var e = window.event;
     e.preventDefault();
 
     var url = e.target.href;
-    url += randomString(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    url += '?=' + randomString(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
     location.replace(url);
 

868fbbcf09b1dc20a0ee7cb47b667d8a1566d29f
Adrian Wisnicki <awisnicki@yahoo.com>
2020-12-18 09:16:52 -0600

    update scripts for dev test

--- scripts/scripts-for-dev.js
+++ scripts/scripts-for-dev.js
@@ -277,6 +277,12 @@ function AddRelNoopener(){
 }
 
 // Adapted from https://stackoverflow.com/a/18704113
+function randomString(length, chars) {
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+  return result;
+}
+
 function randomizeHref()
 {
     var e = window.event;

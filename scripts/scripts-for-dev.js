8cecbb56cb06ad743d61b05a7c15633ad5dead07
Adrian Wisnicki <awisnicki@yahoo.com>
2020-12-18 09:45:57 -0600

    update scripts for dev

--- scripts/scripts-for-dev.js
+++ scripts/scripts-for-dev.js
@@ -266,9 +266,9 @@ function getPreviousElement(items, currentItem){
 /* Adapted from https://social.technet.microsoft.com/Forums/en-US/809eaecb-fc3b-40e2-ae0b-f2d79feb58b0/need-easy-way-to-force-all-links-to-open-in-new-tab */
 /* Needed to prevent a "Best Practices" issue created by Google Translate */
 
-AddRelNoopener();
+AddRelNoopener_RandomizeHref();
 
-function AddRelNoopener(){
+function AddRelNoopener_RandomizeHref(){
     var links = document.querySelectorAll("a");
     for(var i = 0; i < links.length; i++){
         links[i].setAttribute("rel","noopener");

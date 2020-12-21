9f26cc5431ba642c2b4523db3babb23f6f0231a2
Adrian Wisnicki <awisnicki@yahoo.com>
2020-12-21 06:38:29 -0600

    update scripts for dev to remove thanks

--- scripts/scripts-for-dev.js
+++ scripts/scripts-for-dev.js
@@ -276,14 +276,16 @@ function AddRelNoopener(){
 }
 
 
-Thanks();
+/* Commented out b/c breaks external LO links */
 
-function Thanks(){
-    var links = document.querySelectorAll("a[href^='http']:not([href^='#']):not([href^='mailto']):not([onclick])");
-    for(var i = 0; i < links.length; i++){
-      links[i].href = links[i].href + "?=Thanks_for_visiting_One_More_Voice";
-    }
-}
+// Thanks();
+
+// function Thanks(){
+//     var links = document.querySelectorAll("a[href^='http']:not([href^='#']):not([href^='mailto']):not([onclick])");
+//     for(var i = 0; i < links.length; i++){
+//       links[i].href = links[i].href + "?=Thanks_for_visiting_One_More_Voice";
+//     }
+// }
 
 
 /* Link cache buster: Takes all site links, changes them from relative to absolute links (if relative in the first place; absolute links stay absolute), adds a random string to the end. */

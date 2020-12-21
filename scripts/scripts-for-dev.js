0008564dd7a11da7be52761f1c6866838621c618
Adrian Wisnicki <awisnicki@yahoo.com>
2020-12-20 21:26:26 -0600

    update scripts for dev

--- scripts/scripts-for-dev.js
+++ scripts/scripts-for-dev.js
@@ -275,6 +275,7 @@ function AddRelNoopener(){
     }
 }
 
+
 /* Link cache buster: Takes all site links, changes them from relative to absolute links (if relative in the first place; absolute links stay absolute), adds a random string to the end. */
 
 RandomiseHref();
@@ -284,5 +285,15 @@ function RandomiseHref(){
     for(var i = 0; i < links.length; i++){
       var randomString = Math.floor(Math.random()*1000000);
       links[i].href = links[i].href + "?=" + randomString;
+    }
+}
+
+
+Thanks();
+
+function Thanks(){
+    var links = document.querySelectorAll("a:([href^='http']):not([href^='#']):not([href^='mailto']):not([onclick])");
+    for(var i = 0; i < links.length; i++){
+      links[i].href = links[i].href + "?=thanks4visitingOneMoreVoice";
     }
 }

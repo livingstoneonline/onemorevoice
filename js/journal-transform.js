9809171668c4a07704e5eb7f0660b98c9e728d6c
Adrian Wisnicki <awisnicki@yahoo.com>
2021-02-01 13:39:15 -0600

    auto indent all files

--- js/journal-transform.js
+++ js/journal-transform.js
@@ -3,46 +3,46 @@
 
 // Wait till document is loaded before executing.
 document.addEventListener("DOMContentLoaded", function(event) {
-    // Select the button element using it's ID.
-    document.querySelector('input#invert')
-    // Call this function when the button is clicked.
-    .onclick = function () {
-        // Find the element with class transcription,
-        // and toggle the class 'change-display'.
-        document.querySelector('body.transcription')
-        .classList
-        .toggle('invert-color');
-        var input = document.querySelector('input#invert')
-        if (input.checked == true) {
-            input.setAttribute('aria-checked', 'true');
-            input.setAttribute('checked', '');
-        } 
-        else {
-            input.setAttribute('aria-checked', 'false');
-            input.removeAttribute('checked', '');
-        }
-    };
+	// Select the button element using it's ID.
+	document.querySelector('input#invert')
+	// Call this function when the button is clicked.
+	.onclick = function () {
+		// Find the element with class transcription,
+		// and toggle the class 'change-display'.
+		document.querySelector('body.transcription')
+		.classList
+		.toggle('invert-color');
+		var input = document.querySelector('input#invert')
+		if (input.checked == true) {
+			input.setAttribute('aria-checked', 'true');
+			input.setAttribute('checked', '');
+		} 
+		else {
+			input.setAttribute('aria-checked', 'false');
+			input.removeAttribute('checked', '');
+		}
+	};
 });
 
 // Wait till document is loaded before executing.
 document.addEventListener("DOMContentLoaded", function(event) {
-    // Select the button element using it's ID.
-    document.querySelector('input#remove-justification')
-    // Call this function when the button is clicked.
-    .onclick = function () {
-        // Find the element with class transcription,
-        // and toggle the class 'change-display'.
-        document.querySelector('body.transcription')
-        .classList
-        .toggle('unjustify');
-        var input = document.querySelector('input#remove-justification')
-        if (input.checked == true) {
-            input.setAttribute('aria-checked', 'true');
-            input.setAttribute('checked', '');
-        } 
-        else {
-            input.setAttribute('aria-checked', 'false');
-            input.removeAttribute('checked', '');
-        }
-    };
+	// Select the button element using it's ID.
+	document.querySelector('input#remove-justification')
+	// Call this function when the button is clicked.
+	.onclick = function () {
+		// Find the element with class transcription,
+		// and toggle the class 'change-display'.
+		document.querySelector('body.transcription')
+		.classList
+		.toggle('unjustify');
+		var input = document.querySelector('input#remove-justification')
+		if (input.checked == true) {
+			input.setAttribute('aria-checked', 'true');
+			input.setAttribute('checked', '');
+		} 
+		else {
+			input.setAttribute('aria-checked', 'false');
+			input.removeAttribute('checked', '');
+		}
+	};
 });

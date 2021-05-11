164ebaf21f27166fe701933686b313e5e6cb61d0
Adrian Wisnicki <awisnicki@yahoo.com>
2021-05-11 18:06:42 -0500

    reorg scripts & hide top button

--- js/scripts.js
+++ js/scripts.js
@@ -25,53 +25,6 @@ if ("serviceWorker" in navigator) {
 };
 
 
-// Overlay
-// Adapted from https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp
-
-function openNav() {
-	document.getElementById("nav7").style.display = "block";
-	document.getElementById("nav7").style.visibility = "visible";
-	document.getElementsByTagName("html")[0].style.overflowY = "hidden";
-};
-
-function closeNav() {
-	document.getElementById("nav7").style.display = "none";
-	document.getElementById("nav7").style.visibility = "hidden";
-	document.getElementsByTagName("html")[0].style.overflowY = "unset";
-};
-
-
-// Trap Focus in Overlay
-// Adapted from https://www.taraprasad.com/trap-focus-inside-an-element/
-// Also see https://hiddedevries.nl/en/blog/2017-01-29-using-javascript-to-trap-focus-in-an-element
-jQuery(document).ready(function () {
-	jQuery('#overlay-last').on('keydown', function (e) {
-		var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
-		if (!isTabPressed) {
-			return
-		}
-		if (e.shiftKey) {
-			return
-		} else {
-			jQuery('#nav8').focus();
-			e.preventDefault()
-		}
-			});
-	jQuery('#nav8').on('keydown', function (e) {
-		var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
-		if (!isTabPressed) {
-			return
-		}
-		if (e.shiftKey) {
-			jQuery('#overlay-last').focus();
-			e.preventDefault()
-		} else {
-			return
-		}
-	})
-});
-
-
 // Keyboard Navigation for Dropdown Menus
 // Adapted by Philip Allfrey for One More Voice from https://www.w3.org/TR/wai-aria-practices/examples/menubar/menubar-1/js/MenubarItemLinks.js
 
@@ -281,36 +234,8 @@ function getPreviousElement(items, currentItem){
 };
 
 
-// Adapted from https://social.technet.microsoft.com/Forums/en-US/809eaecb-fc3b-40e2-ae0b-f2d79feb58b0/need-easy-way-to-force-all-links-to-open-in-new-tab
-// Needed to prevent a "Best Practices" issue created by Google Translate
-
-AddRelNoopener();
-
-function AddRelNoopener(){
-		var links = document.querySelectorAll("a");
-		for(var i = 0; i < links.length; i++){
-				links[i].setAttribute("rel","noopener");
-		}
-};
-
-
-// Link cache buster: Takes all site links, changes them from relative to absolute links (if relative in the first place; absolute links stay absolute), adds a random string to the end.
-// Note: Add random string bit currently disabled.
-
-RandomiseHref();
-
-function RandomiseHref(){
-		var links = document.querySelectorAll("a:not([href^='http']):not([href*='#']):not([href^='mailto']):not([onclick]):not([class='trans-return']):not([class='art-return'])");
-		for(var i = 0; i < links.length; i++){
-			// var randomString = Math.floor(Math.random()*1000000);
-			// links[i].href = links[i].href + "?=" + randomString;
-			links[i].href = links[i].href;
-		}
-};
-
-
-// Adapted from https://stackoverflow.com/a/21718316
 // Highlights users current section in navigation
+// Adapted from https://stackoverflow.com/a/21718316
 
 checkUrl();
 			
@@ -347,24 +272,126 @@ function checkUrl () {
 };	
 
 
-// Adapted from https://stackoverflow.com/a/30073090
+//Scroll to top button
+//Adapted from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+//Get the button
+// var mybutton = document.getElementById("topButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+//     mybutton.style.display = "block";
+//     mybutton.style.visibility = "visible";
+//   } 
+//   else {
+//     mybutton.style.display = "none";
+//     mybutton.style.visibility = "hidden";
+//   }
+// };
+			
+// When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+// document.body.scrollTop = 0;
+// document.documentElement.scrollTop = 0;
+// };
+
+
+// Overlay
+// Adapted from https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp
+
+function openNav() {
+	document.getElementById("nav7").style.display = "block";
+	document.getElementById("nav7").style.visibility = "visible";
+	document.getElementsByTagName("html")[0].style.overflowY = "hidden";
+};
+
+function closeNav() {
+	document.getElementById("nav7").style.display = "none";
+	document.getElementById("nav7").style.visibility = "hidden";
+	document.getElementsByTagName("html")[0].style.overflowY = "unset";
+};
+
+
+// Trap Focus in Overlay
+// Adapted from https://www.taraprasad.com/trap-focus-inside-an-element/
+// Also see https://hiddedevries.nl/en/blog/2017-01-29-using-javascript-to-trap-focus-in-an-element
+jQuery(document).ready(function () {
+	jQuery('#overlay-last').on('keydown', function (e) {
+		var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+		if (!isTabPressed) {
+			return
+		}
+		if (e.shiftKey) {
+			return
+		} else {
+			jQuery('#nav8').focus();
+			e.preventDefault()
+		}
+			});
+	jQuery('#nav8').on('keydown', function (e) {
+		var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+		if (!isTabPressed) {
+			return
+		}
+		if (e.shiftKey) {
+			jQuery('#overlay-last').focus();
+			e.preventDefault()
+		} else {
+			return
+		}
+	})
+});
+
+
+// Needed to prevent a "Best Practices" issue created by Google Translate
+// Adapted from https://social.technet.microsoft.com/Forums/en-US/809eaecb-fc3b-40e2-ae0b-f2d79feb58b0/need-easy-way-to-force-all-links-to-open-in-new-tab
+
+AddRelNoopener();
+
+function AddRelNoopener(){
+		var links = document.querySelectorAll("a");
+		for(var i = 0; i < links.length; i++){
+				links[i].setAttribute("rel","noopener");
+		}
+};
+
+
+// Link cache buster: Takes all site links, changes them from relative to absolute links (if relative in the first place; absolute links stay absolute), adds a random string to the end.
+// Note: Add random string bit currently disabled.
+
+RandomiseHref();
+
+function RandomiseHref(){
+		var links = document.querySelectorAll("a:not([href^='http']):not([href*='#']):not([href^='mailto']):not([onclick]):not([class='trans-return']):not([class='art-return'])");
+		for(var i = 0; i < links.length; i++){
+			// var randomString = Math.floor(Math.random()*1000000);
+			// links[i].href = links[i].href + "?=" + randomString;
+			links[i].href = links[i].href;
+		}
+};
+
+
 // Removes unused Google script that also registers an unload listener
+// Adapted from https://stackoverflow.com/a/30073090
 
 $('head').find('script').filter(function(){
     return $(this).attr('src') === 'https://translate.googleapis.com/element/TE_20201130_00/e/js/element/element_main.js'
 }).remove();
 
 
-// Adapted from https://stackoverflow.com/a/30073090
 // Ensures that Google logo for Translate is served at correct size
+// Adapted from https://stackoverflow.com/a/30073090
 
 $('body').find('img').filter(function(){
     return $(this).attr('src') === 'https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_42x16dp.png'
 }).replaceWith( "<img src='https://www.gstatic.com/images/branding/googlelogo/2x/googlelogo_color_42x16dp.png' srcset='https://www.gstatic.com/images/branding/googlelogo/2x/googlelogo_color_42x16dp.png 2x, https://www.gstatic.com/images/branding/googlelogo/1x/googlelogo_color_42x16dp.png 1x' sizes='38.5px' width='38.5px' height='14.656px' style='padding-right:3px;' alt='Google Translate' />" );
 
 
-// Taken from https://stackoverflow.com/a/28840664 and https://stackoverflow.com/a/48542058
 // Reloads given page, keeps base URL, path, and any #, but removes random query string
+// Taken from https://stackoverflow.com/a/28840664 and https://stackoverflow.com/a/48542058
 
 // (function () {
 //     if (window.localStorage) {

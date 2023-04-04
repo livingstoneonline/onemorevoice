a7c25f267fd3b66c19bc70a43914c3489bd8bb95
awisnicki <awisnicki@yahoo.com>
2023-04-03 21:03:12 -0500

    u replace-fa js

--- js/replace-fa-icons.js
+++ js/replace-fa-icons.js
@@ -20,54 +20,57 @@ document.addEventListener('DOMContentLoaded', () => {
           }
       })
   });
-  function replaceIconsWithText() {
-      const icons = [
-          {
-              selector: ".fa-bars",
-              text: "Menu"
-          }, {
-              selector: ".fa-times",
-              text: "Close"
-          }, {
-              selector: ".fa-map-marker",
-              text: "Site Map"
-          }, {
-              selector: ".fa-home",
-              text: "Home"
-          }, {
-              selector: ".fa-envelope",
-              text: "Email"
-          }, {
-              selector: ".fa-github-alt",
-              text: "GitHub"
-          }, {
-              selector: ".fa-zotero",
-              text: "Zotero"
-          }
-      ];
-      const navContainer = document.querySelector(".nav-container");
-      if (navContainer) {
-          icons.forEach(icon => {
-              const elements = navContainer.querySelectorAll(icon.selector);
-              elements.forEach(element => {
-                  const textNode = document.createTextNode(icon.text);
-                  element
-                      .parentNode
-                      .replaceChild(textNode, element)
-              })
-          })
-      }
-  }
-  function hideElementsWithClasses() {
-      const classes = ["fa", "fas", "fa-solid", "fa-brands", "caption-icon"];
-      const bodyElements = document.querySelectorAll("body *:not(.nav-container)");
-      bodyElements.forEach(element => {
-          classes.forEach(className => {
-              if (element.classList.contains(className)) {
-                  element.style.display = "none";
-                  element.style.visibility = "hidden"
-              }
-          })
-      })
-  }
+    function replaceIconsWithText() {
+        const icons = [
+            {
+                selector: ".fa-bars",
+                text: "Menu"
+            }, {
+                selector: ".fa-times",
+                text: "Close"
+            }, {
+                selector: ".fa-map-marker",
+                text: "Site Map"
+            }, {
+                selector: ".fa-home",
+                text: "Home"
+            }, {
+                selector: ".fa-envelope",
+                text: "Email"
+            }, {
+                selector: ".fa-github-alt",
+                text: "GitHub"
+            }, {
+                selector: ".fa-zotero",
+                text: "Zotero"
+            }
+        ];
+        const navContainer = document.querySelector(".nav-container");
+        if (navContainer) {
+            icons.forEach(icon => {
+                const elements = navContainer.querySelectorAll(icon.selector);
+                elements.forEach(element => {
+                    const textNode = document.createTextNode(icon.text);
+                    const newTextEl = document.createElement("span");
+                    newTextEl.style.textDecoration = "underline";
+                    newTextEl.appendChild(textNode);
+                    element
+                        .parentNode
+                        .replaceChild(newTextEl, element)
+                })
+            })
+        }
+    }
+    function hideElementsWithClasses() {
+        const classes = ["fa", "fas", "fa-solid", "fa-brands", "caption-icon"];
+        const bodyElements = document.querySelectorAll("body *:not(.nav-container)");
+        bodyElements.forEach(element => {
+            classes.forEach(className => {
+                if (element.classList.contains(className)) {
+                    element.style.display = "none";
+                    element.style.visibility = "hidden"
+                }
+            })
+        })
+    }
 });
